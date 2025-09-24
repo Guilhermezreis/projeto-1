552c9c844d865e1eae5dd07e6a7ff7cf40f2eb9d
Raulpenido <raul.goncalvepenido@gmail.com>
2025-09-24 16:18:49 -0300

    Update sgfd.c

--- sgfd.c
+++ sgfd.c
@@ -1,26 +1,38 @@
-#include <stdio.h>
+/******************************************************************************
 
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
-	int jogador, maquina;
-	printf(" 1 = papel\n 2 = pedra\n 3 = tesoura\n");
-	scanf("%d", &jogador);
-	maquina = rand() % 3;
-	if (jogador == maquina){
-	    printf("empate");
-	}else if (jogador == 1 && maquina == 2){
-	    printf("jogador ganhou");
-	}else if (jogador == 1 && maquina == 3){
-	    printf("maquina ganhou");
-	}else if (jogador == 2 && maquina == 1){
-	    printf("maquina ganhou");
-	}else if (jogador == 2 && maquina == 3){
-	    printf("jogador ganhou");
-	}else if (jogador == 3 && maquina == 2){
-	    printf("maquina ganhou");
-	}else if (jogador == 3 && maquina == 1){
-	    printf("jogador ganhou");
-	}
+    srand(time(NULL));
+    int jogador, maquina;
+    char continuar;
+    do{
+    printf("0: Pedra 1: Papel: 2 Tesoura\n");
+    scanf("%d", &jogador);
+    maquina = rand() % 3;
+    if(jogador == maquina){
+        printf("Empate");
+    }
+    else if(jogador == 0 && maquina == 2 || jogador == 1 && maquina == 0 || jogador == 2 && maquina == 1){
+        printf("Ganhou");
+    }
+    else{
+        printf("Perdeu");
+    }
+    printf("\nQuer parar?\n");
+    scanf("%s", &continuar);
+    
+    }while( continuar != 's' && continuar != 'S');
+    
+    
 
-	return 0;
+    return 0;
 }
